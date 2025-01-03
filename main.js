3cb58041e095609d145a0c9649560e31b88466a5
Akshai Dev <akshaimuraleedharan4@gmail.com>
2025-01-03 15:53:23 +0000

    game first test

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ const warRoomBottom = document.querySelector('.war_room_bottom_image')
 
 
 
-const screenWidth = screen.width 
+const screenWidth = window.innerWidth 
 
     let initialLength = 0
     let warTopInterval = setInterval(() => {
-    let randomNum = Math.floor(Math.random() * 30)
+    let randomNum = Math.floor(Math.random() *  screenWidth > 1000 ? 80 : 30)
     let changeValue = initialLength += randomNum
     warRoomTop.style.transform = `translateX(${changeValue}px)`
-    warRoomTop.style.transition = "transform 0.5s ease-in"
+    warRoomTop.style.transition = `transform ${screenWidth > 1000 ? '0.2s' : '0.5s'} ease-in`
 
         if( initialLength > screenWidth - 140){
          let clearInter =  setInterval(() => {
-            let randomNum = Math.floor(Math.random() * 30)
+         
             let changeValue = initialLength -= randomNum
             warRoomTop.style.transform = `translateX(${changeValue}px)`
-            warRoomTop.style.transition = "transform 0.5s ease-in"
-            if(initialLength <= 40){
+            warRoomTop.style.transition = `transform ${screenWidth > 1000 ? '0.2s' : '0.5s'} ease-in`
+            if(initialLength <= 60){
                 clearInterval(clearInter)
                }
            },2000)
@@ -30,18 +30,21 @@ const screenWidth = screen.width
 
 let initialLengthBottom = 0
 let warBottomInterval = setInterval(() => {
-let randomNum = Math.floor(Math.random() * 30)
+let randomNum = Math.floor(Math.random() *  screenWidth > 1000 ? 80 : 30)
+
+
 let changeValue = initialLengthBottom += randomNum
 warRoomBottom.style.transform = `translateX(${changeValue}px)`
-warRoomBottom.style.transition = "transform 0.5s ease-in"
+warRoomBottom.style.transition = `transform ${screenWidth > 1000 ? '0.2s' : '0.5s'} ease-in`
+    
 
-    if( initialLength > screenWidth - 120){
+    if( initialLengthBottom > screenWidth - 140){
      let clearInter =  setInterval(() => {
-        let randomNum = Math.floor(Math.random() * 30)
+      
         let changeValue = initialLengthBottom -= randomNum
         warRoomBottom.style.transform = `translateX(${changeValue}px)`
-        warRoomBottom.style.transition = "transform 0.5s ease-in"
-        if(initialLength <= 40){
+        warRoomBottom.style.transition = `transform ${screenWidth > 1000 ? '0.2s' : '0.5s'} ease-in`
+        if(initialLength <= 60){
             clearInterval(clearInter)
            }
        },2000)
